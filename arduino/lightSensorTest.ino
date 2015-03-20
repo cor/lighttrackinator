d027028396486e707d2c24580339f42881c74e88
Leon <Leongrasmeijer@gmail.com>
2015-03-20 15:32:48 +0100

    Added lightsensortest

---
+++ arduino/lightSensorTest.ino
@@ -0,0 +1,19 @@
+int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
+int photocellReading;     // the analog reading from the sensor divider
+int LEDpin = 11;          // connect Red LED to pin 11 (PWM pin)
+int LEDbrightness;        // 
+void setup(void) {
+  // We'll send debugging information via the Serial monitor
+  Serial.begin(9600);   
+}
+ 
+void loop(void) {
+  photocellReading = analogRead(photocellPin);  
+ 
+  Serial.print("Analog reading = ");
+  Serial.println(photocellReading);     // the raw analog reading
+  
+  analogWrite(LEDpin, photocellReading > 299 ? 0 : 255);
+  
+  delay(100);
+}
